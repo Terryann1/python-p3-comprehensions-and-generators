68d3e5975ee0625a39f9dabd277fd39f86c5b178
+ <+>
2025-02-13 13:34:59 +0300

    list comprehensions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    returns_evens = [n for n in num_list if n % 2 == 0]
+    return returns_evens
     pass
 
 def make_exclamation(sentence_list):
+    
+    return [sentence + '!' for sentence in sentence_list]
     pass
